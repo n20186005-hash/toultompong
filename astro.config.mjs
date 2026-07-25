3f90453fdb5ba1028332d4c7278e3b55cb2baa5f
n20186005-hash <n20186005@gmail.com>
2026-07-25 23:10:11 +0800

    更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://toultompongmarket.com',
-  i18n: {
-    defaultLocale: 'km',
-    locales: ['km', 'en', 'zh'],
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
   vite: {
     plugins: [tailwindcss()],
   },
